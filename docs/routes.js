1b8e0cec35486243f9fd2b6b6ead28dfc95d56d0
猴子 <70706886+a3510377@users.noreply.github.com>
2022-05-25 18:12:08 +0800

    style: edit code style

--- docs/routes.js
+++ docs/routes.js
@@ -1,4 +1,4 @@
 export default [
-  { text: "Python版本 (discord.py)", link: "/python/" },
-  { text: "JavaSript版本 (discord.js)", link: "/javascript/" },
-]
+  { text: 'Python版本 (discord.py)', link: '/python/' },
+  { text: 'JavaSript版本 (discord.js)', link: '/javascript/' },
+];
