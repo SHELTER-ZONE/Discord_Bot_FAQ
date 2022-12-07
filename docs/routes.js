b3e32a37ed1cb3f8921956db1b1fc040c2c991a9
Xzihnago <xzihnago@gmail.com>
2022-12-07 19:34:12 +0800

    update: routes

--- docs/routes.js
+++ docs/routes.js
@@ -3,7 +3,13 @@ const pythonSidebar = [{ text: 'Python版本 (discord.py)', link: '/python/' }]
 const javaScriptSidebar = [
   {
     text: 'JavaSript版本 (discord.js)',
-    children: [{ text: '入門', link: '/javascript/getting-started/' }],
+    children: [
+      { text: '安裝與準備', link: '/javascript/preparations/' },
+      { text: '入門', link: '/javascript/getting-started/' },
+      { text: '常見問題', link: '/javascript/problems/' },
+      { text: '網關意圖', link: '/javascript/intents/' },
+      { text: '安裝Node.js', link: '/javascript/installing-nodejs/' }
+    ],
   },
 ]
 
