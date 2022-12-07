14ee71768e0c1c12ed2289fea85d342a59b68258
Xzihnago <xzihnago@gmail.com>
2022-12-07 22:53:05 +0800

    update: routes

--- docs/routes.js
+++ docs/routes.js
@@ -6,6 +6,8 @@ const javaScriptSidebar = [
     children: [
       { text: '安裝與準備', link: '/javascript/preparations/' },
       { text: '入門', link: '/javascript/getting-started/' },
+      { text: '事件', link: '/javascript/events/' },
+      { text: '指令', link: '/javascript/commands/' },
       { text: '常見問題', link: '/javascript/problems/' },
       { text: '網關意圖', link: '/javascript/intents/' },
       { text: '安裝Node.js', link: '/javascript/installing-nodejs/' }
